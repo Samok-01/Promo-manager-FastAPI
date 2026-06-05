834dce2f05b768d615ad1b972e3a6404fe2ffc2d
Samok_PC <rymbekov.samgaa@gmail.com>
2026-06-06 00:05:48 +0500

    Move database to docker-compose PostgreSQL service

--- database.py
+++ database.py
@@ -1,12 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./promos.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./promos.db")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
